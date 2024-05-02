4078af268f71a8507b79f9b40d7030e0087e27f5
Rob Blake <rob.bl8ke@outlook.com>
2024-05-02 15:02:34 +0200

    Do a simple diagnostics scan of files and settings

--- sub-scripts/diagnostics/diagnostics.ps1
+++ sub-scripts/diagnostics/diagnostics.ps1
@@ -1,8 +1,37 @@
 Write-Host "Running diagnostics check..."
 Write-Host "Scanning..."
-Start-Sleep -Milliseconds 10000
+
+$issues = @()
+
+$jsonPath = "C:\Code\rob-bl8ke\Menu-Terminal\data\diagnostics\secrets.json"
+$json = Get-Content -Path $jsonPath | ConvertFrom-Json
+
+if ($null -eq $json.database.connectionString) {
+    $issues = $issues + "- Connection string setting does not exist in secrets file."
+}
+elseif ($json.database.connectionString -eq "[Connection-String]") {
+    $issues = $issues + "- Connection string is not set in secrets file."  
+}
+
+$settingsPath = "C:\Code\rob-bl8ke\Menu-Terminal\data\diagnostics\settings.xml"
+$xml = [xml](Get-Content $settingsPath)
+
+$node = $xml.SelectNodes("//ApplicationManifest/Parameters/Parameter")
+# or
+# $node = $xml.ApplicationManifest.Parameters.Parameter `
+# | Where-Object { $_.Name -eq "Parameter" } | Select-Object -First 1
+# or
+# $node = $xml.ApplicationManifest.Parameters.Parameter
+
+if ($node.DefaultName -eq "Platform-Key") {
+    $issues = $issues + "- Platform-Key placeholder has not been replaced in settings file." 
+}
+
+Start-Sleep -Milliseconds 2000
 Write-Host "... scanning completed. The following issues have been found:"
-Write-Host "- FunctionsApp does not seem to have been configured."
-Write-Host "- Customer Service config does not seem to have been configured."
-Write-Host "- Service Fabric not started."
+Write-Host ""
+
+$issues | Format-Table
+Write-Host ""
+
 pause
